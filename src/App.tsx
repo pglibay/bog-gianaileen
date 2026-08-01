25ae9bf779194f347de7bcca5f6dc4c6852a929f
Phillip Glenn Libay <3772496+pglibay@users.noreply.github.com>
2026-08-01 16:21:24 +0800

    BOG: Gian & Aileen wedding, first commit.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1 className="text-3xl font-bold underline">
+          Gian & Aileen
+        </h1>
+      </section>
+    </>
+  )
+}
+
+export default App
